d0cfdd875a4eab0f8efb5f016f90dd478002cf88
Maco <Maco@LAPTOP-O1JLL8OB>
2020-05-26 16:45:27 +0200

    Completo - Visite grafi e cammini minimi

--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -1,13 +1,23 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
 import org.jgrapht.Graphs;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.DepthFirstIterator;
+import org.jgrapht.traverse.GraphIterator;
 
 import it.polito.tdp.metroparis.db.MetroDAO;
 
@@ -44,8 +54,8 @@ public class Model {
 			}
 		}
 		
-		//System.out.println(this.graph);
-		System.out.format("grafo caricato con con %d vertici e %d archi", this.graph.vertexSet().size(), this.graph.edgeSet().size());*/
+		//System.out.println(this.graph);*/
+		
 		
 		//-metodo 2 (da un vertice, trova tutti i connessi)
 		/*for(Fermata fp : this.fermate) {
@@ -54,10 +64,9 @@ public class Model {
 			for(Fermata fa : connesse) {
 				this.graph.addEdge(fp, fa);
 			}
-		}
+		}*/
 		
-		System.out.format("grafo caricato con con %d vertici e %d archi", this.graph.vertexSet().size(), this.graph.edgeSet().size());*/
-		
+				
 		
 		//-metodo 3 (chiedo al database l'elenco degli archi)
 		List<CoppiaFermate>coppie=dao.coppieFermate(fermateIdMap);
@@ -65,11 +74,114 @@ public class Model {
 			this.graph.addEdge(c.getFp(), c.getFa());
 		}
 		
-		System.out.format("grafo caricato con con %d vertici e %d archi", this.graph.vertexSet().size(), this.graph.edgeSet().size());
+		System.out.format("Grafo caricato con con %d vertici e %d archi\n", this.graph.vertexSet().size(), this.graph.edgeSet().size());
 	}
+	
+	/**
+	 * Visita l'intero grafo con la stategia Breadth First 
+	 * e ritorna l'insieme dei vertici incontrati
+	 * @param source vertice di partenza della visita
+	 * @return insieme dei vertici incontrati
+	 */
+	public List<Fermata> visitaAmpiezza(Fermata source) {
+		List<Fermata>visita=new ArrayList<>();
+		GraphIterator<Fermata, DefaultEdge>bfv=new BreadthFirstIterator<>(graph, source);
+		while(bfv.hasNext()) {
+			visita.add(bfv.next());
+		}
+		return visita;
+	}
+	
+	
+	public List<Fermata> visitaProfondita(Fermata source) {
+		List<Fermata>visita=new ArrayList<>();
+		GraphIterator<Fermata, DefaultEdge>dfv=new DepthFirstIterator<>(graph, source);
+		while(dfv.hasNext()) {
+			visita.add(dfv.next());
+		}
+		return visita;
+	}
+	
+	public Map<Fermata, Fermata> alberoVisita(Fermata source) {
+		Map<Fermata, Fermata>albero=new HashMap<>();
+		albero.put(source, null);
+		
+		GraphIterator<Fermata, DefaultEdge>bfv=new BreadthFirstIterator<>(graph, source);
+		bfv.addTraversalListener(new TraversalListener<Fermata, DefaultEdge>(){
+			
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultEdge> e) {
+				// la visita sta considerando un nuovo arco
+				//questo arco ha scoperto un nuovo vertice?
+				//se sì, provenendo da dove?
+				DefaultEdge edge=e.getEdge(); //(a,b) : ho scoperto 'a' partendo da 'b' oppure 'b' da 'a'
+				Fermata a=graph.getEdgeSource(edge);
+				Fermata b=graph.getEdgeTarget(edge);
+				if(albero.containsKey(a)) {
+					albero.put(b,a);
+				}else {
+					albero.put(a,b);
+				}
+
+			}
+
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Fermata> e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Fermata> e) {
+				// TODO Auto-generated method stub
+
+			}
+			
+		});
+		
+		while(bfv.hasNext()) {
+			bfv.next(); //estrai l'elemento e ignoralo
+		}
+		
+		return albero;
+	}
+	
+	
+	public List<Fermata>camminiMinimi(Fermata partenza, Fermata arrivo) {
+		DijkstraShortestPath<Fermata, DefaultEdge> dij=new DijkstraShortestPath<>(graph);
+		
+		GraphPath<Fermata, DefaultEdge>cammino=dij.getPath(partenza, arrivo);
+		
+		return cammino.getVertexList();
+	}
+	
 	
 	public static void main(String args[]) {
 		Model m=new Model();
+		List<Fermata>visita1=m.visitaAmpiezza(m.fermate.get(0));
+		//System.out.println(visita1);
+		List<Fermata>visita2=m.visitaProfondita(m.fermate.get(0));
+		//System.out.println(visita2);
+		
+		Map<Fermata, Fermata>albero=m.alberoVisita(m.fermate.get(0));
+		for(Fermata f : albero.keySet()) {
+			//System.out.format("%s <- %s\n", f, albero.get(f));
+		}
+		
+		List<Fermata>cammino=m.camminiMinimi(m.fermate.get(0), m.fermate.get(1));
+		System.out.println(cammino);
 	}
 
 }
